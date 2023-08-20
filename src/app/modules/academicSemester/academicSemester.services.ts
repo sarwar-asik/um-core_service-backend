e4d59946b2134ffa23adac0407e49f0e5a8cac27
sarwar-asik <sarwarasik@gmail.com>
2023-08-20 10:15:05 +0600

    semese error solved

--- src/app/modules/academicSemester/academicSemester.services.ts
+++ src/app/modules/academicSemester/academicSemester.services.ts
@@ -1,11 +1,9 @@
-import { AcademicSeemestar, PrismaClient } from '@prisma/client';
+import { AcademicSemester, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const insertDB = async (
-  data: AcademicSeemestar
-): Promise<AcademicSeemestar> => {
-  const result = await prisma.academicSeemestar.create({
+const insertDB = async (data: AcademicSemester): Promise<AcademicSemester> => {
+  const result = await prisma.academicSemester.create({
     data,
   });
 
