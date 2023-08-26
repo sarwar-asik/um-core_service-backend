15173edc4d14921823cc9727a4233aed1611c5f4
sarwar-asik <sarwarasik@gmail.com>
2023-08-27 00:41:41 +0600

    index routing added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { academicRouter } from '../modules/academicSemester/academicSemester.router';
 import { studentsRoutes } from '../modules/Students/Students.route';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
+import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 
 const router = express.Router();
 
@@ -13,7 +15,15 @@ const moduleRoutes = [
   {
     path:'/students',
     routes:studentsRoutes
-  }
+  },
+  {
+    path:'/academic-department',
+    routes:academicDepartmentRoutes
+  },
+  {
+    path:'/academic-faculty',
+    routes:academicFacultyRoutes
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route?.routes));
