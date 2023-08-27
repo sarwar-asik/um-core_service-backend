4e6694233b28c2c4f6045165128ca1bab568d69b
sarwar-asik <sarwarasik@gmail.com>
2023-08-27 12:41:28 +0600

    jwt

--- src/app/modules/Students/Students.route.ts
+++ src/app/modules/Students/Students.route.ts
@@ -5,9 +5,12 @@ import { Router } from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { StudentController } from './Students.controller';
 import { StudentsValidation } from './Students.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 const router = Router();
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(StudentsValidation.createStudents),
   StudentController.insertDB
 );
@@ -15,11 +18,13 @@ router.get('/', StudentController.getAllDb);
 
 router.put(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN,ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentsValidation.updateStudents),
   StudentController.updateIntoDb
 );
 router.delete(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
 
   StudentController.deleteFromDb
 );
--- src/app/modules/academicSemester/academicSemester.router.ts
+++ src/app/modules/academicSemester/academicSemester.router.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterValidation } from './academic.validation';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.ADMIN,ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicSemesterValidation.createAcademicSemester),
   AcademicSemesterController.insertDB
 );
