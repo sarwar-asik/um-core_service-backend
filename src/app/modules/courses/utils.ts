135d20ba0108432c53bc954c2a7f9a571de20254
sarwar-asik <sarwarasik@gmail.com>
2023-08-31 16:20:34 +0600

    hasTimeConflict in utils solved

--- src/app/modules/courses/utils.ts
+++ src/app/modules/courses/utils.ts
@@ -1,5 +1,3 @@
-
-
 export const asyncForEach = async (array: any[], callback: any) => {
   if (!Array.isArray(array)) {
     throw new Error('Expect an array');
@@ -10,29 +8,41 @@ export const asyncForEach = async (array: any[], callback: any) => {
 };
 
 export const hasTimeConflict = (
-  existingSlots:{
-    startTime:string,
-    endTime:string,
-    dayOfWeak:string
+  existingSlots: {
+    startTime: string;
+    endTime: string;
+    dayOfWeek: string;
   }[],
 
-  newSlot:{
-    startTime:string,
-    endTime:string,
-    dayOfWeak:string
+  newSlot: {
+    startTime: string;
+    endTime: string;
+    dayOfWeek: string;
   }
 ) => {
   for (const slot of existingSlots) {
-    const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`);
-    const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`);
-    const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
-    const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`);
+    //   const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`);
+    //   const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`);
+    //   const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
+    //   const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`);
 
-    if (newStartTime <= existingStartTime && newEndTime >= existingEndTime) {
-      // throw new ApiError(httpStatus.CONFLICT, 'The Room already booked  ');
-      // console.log('the room booked');
-      return true
+    //   console.log('the room not booked','eeeeee',existingSlots,"nnnnnn",newSlot);
+
+    //   if (newStartTime < existingStartTime && newEndTime > existingEndTime) {
+    //     // throw new ApiError(httpStatus.CONFLICT, 'The Room already booked  ');
+    //     console.log('the room booked');
+    //     return true
+    //   }
+    //   return false
+    // }
+
+    const existingStart = new Date(`1970-01-01T${slot.startTime}:00`);
+    const existingEnd = new Date(`1970-01-01T${slot.endTime}:00`);
+    const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
+    const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
+
+    if (newStart < existingEnd && newEnd > existingStart) {
+      return true;
     }
-    return false
   }
 };
--- src/app/modules/offeredCourseClassSchedules/offeredCourseClassSchedules.service.ts
+++ src/app/modules/offeredCourseClassSchedules/offeredCourseClassSchedules.service.ts
@@ -1,63 +1,64 @@
+import { OfferedCourseClassSchedulesUtils } from './offeredCourseClassSchedules.utils';
 import { OfferedCourseClassSchedule} from '@prisma/client';
 
 import prisma from '../../../shared/prisma';
-import ApiError from '../../../errors/ApiError';
-import httpStatus from 'http-status';
-import { hasTimeConflict } from '../courses/utils';
+
 
 const insertDB = async (
   data: OfferedCourseClassSchedule
 ): Promise<OfferedCourseClassSchedule> => {
 
-const alreadyBookedRoomOnDay = await prisma.offeredCourseClassSchedule.findMany({
-  where:{
-    dayOfWeek:data?.dayOfWeek,
-    room:{
-      id:data?.roomId
-    }
-  }
-})
 
-console.log(alreadyBookedRoomOnDay,'aaaaaaaaaa');
-
-const existingSlots = alreadyBookedRoomOnDay.map((schedules)=>({
-  startTime:schedules.startTime,
-  endTime:schedules.endTime,
-  dayOfWeak:schedules.dayOfWeek
-
-}))
-
-console.log(existingSlots,"exiiiiiiiii");
-const newSlot ={
-  startTime :data?.startTime,
-  endTime:data?.endTime,
-  dayOfWeak:data?.dayOfWeek
-
-}
-console.log(newSlot,"newwwwwwwwwwww");
+  await OfferedCourseClassSchedulesUtils.checkRoomAvailable(data)
+  await OfferedCourseClassSchedulesUtils.checkFacultyAvailable(data)
 
 
-
-// for ( const  slot of existingSlots){
-//   const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`)
-//   const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`)
-//   const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`)
-//   const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`)
-
-//   if(newStartTime  <= existingStartTime && newEndTime >= existingEndTime){
-//     throw new ApiError(httpStatus.CONFLICT,"The Room already booked  ")
-//     // console.log('the room booked');
+// const alreadyBookedRoomOnDay = await prisma.offeredCourseClassSchedule.findMany({
+//   where:{
+//     dayOfWeek:data?.dayOfWeek,
+//     room:{
+//       id:data?.roomId
+//     }
 //   }
+// })
 
+// console.log(alreadyBookedRoomOnDay,'aaaaaaaaaa');
+
+// const existingSlots = alreadyBookedRoomOnDay.map((schedules)=>({
+//   startTime:schedules.startTime,
+//   endTime:schedules.endTime,
+//   dayOfWeek:schedules.dayOfWeek
+
+// }))
+
+// console.log(existingSlots,"exiiiiiiiii");
+// const newSlot ={
+//   startTime :data?.startTime,
+//   endTime:data?.endTime,
+//   dayOfWeek:data?.dayOfWeek
 
 // }
+// console.log(newSlot,"newwwwwwwwwwww");
 
 
-if(hasTimeConflict(existingSlots,newSlot)){
-  throw new ApiError(httpStatus.CONFLICT,"The Room already booked  ")
-}
+
+// // for ( const  slot of existingSlots){
+// //   const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`)
+// //   const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`)
+// //   const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`)
+// //   const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`)
+
+// //   if(newStartTime  <= existingStartTime && newEndTime >= existingEndTime){
+// //     throw new ApiError(httpStatus.CONFLICT,"The Room already booked  ")
+// //     // console.log('the room booked');
+// //   }
 
 
+// // }
+
+// if(hasTimeConflict(existingSlots,newSlot)){
+//   throw new ApiError(httpStatus.CONFLICT,"The Room already booked  ")
+// }
 
   const result = await prisma.offeredCourseClassSchedule.create({
     data,
@@ -73,4 +74,6 @@ if(hasTimeConflict(existingSlots,newSlot)){
   return result;
 };
 
-export const OfferedCourseClassSchedulesService = { insertDB };
+
+
+export const OfferedCourseClassSchedulesService = { insertDB};
