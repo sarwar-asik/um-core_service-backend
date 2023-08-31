f565d092c3d2dce07777374981ef4ac813bf0123
sarwar-asik <sarwarasik@gmail.com>
2023-08-31 23:28:48 +0600

    semester registrati

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -9,7 +9,11 @@ import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
-import { semesterRegistrationRelationalFields, semesterRegistrationRelationalFieldsMapper, semesterRegistrationSearchableFields } from './semesterRegistration.constant';
+import {
+  semesterRegistrationRelationalFields,
+  semesterRegistrationRelationalFieldsMapper,
+  semesterRegistrationSearchableFields,
+} from './semesterRegistration.constant';
 
 const insertDB = async (
   data: SemesterRegistration
@@ -40,16 +44,10 @@ const insertDB = async (
   return result;
 };
 
-
-
-
-
-
 type ISemesterRegistrationFilterRequest = {
   searchTerm?: string | undefined;
-    academicSemesterId?: string | undefined;
+  academicSemesterId?: string | undefined;
 };
-
 
 const getAllFromDB = async (
   filters: ISemesterRegistrationFilterRequest,
@@ -61,165 +59,199 @@ const getAllFromDB = async (
   const andConditions = [];
 
   if (searchTerm) {
-      andConditions.push({
-          OR: semesterRegistrationSearchableFields.map((field) => ({
-              [field]: {
-                  contains: searchTerm,
-                  mode: 'insensitive'
-              }
-          }))
-      });
+    andConditions.push({
+      OR: semesterRegistrationSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
   }
 
   if (Object.keys(filterData).length > 0) {
-      andConditions.push({
-          AND: Object.keys(filterData).map((key) => {
-              if (semesterRegistrationRelationalFields.includes(key)) {
-                  return {
-                      [semesterRegistrationRelationalFieldsMapper[key]]: {
-                          id: (filterData as any)[key]
-                      }
-                  };
-              } else {
-                  return {
-                      [key]: {
-                          equals: (filterData as any)[key]
-                      }
-                  };
-              }
-          })
-      });
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (semesterRegistrationRelationalFields.includes(key)) {
+          return {
+            [semesterRegistrationRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
   }
 
   const whereConditions: Prisma.SemesterRegistrationWhereInput =
-      andConditions.length > 0 ? { AND: andConditions } : {};
+    andConditions.length > 0 ? { AND: andConditions } : {};
 
   const result = await prisma.semesterRegistration.findMany({
-      include: {
-          academicSemester: true
-      },
-      where: whereConditions,
-      skip,
-      take: limit,
-      orderBy:
-          options.sortBy && options.sortOrder
-              ? { [options.sortBy]: options.sortOrder }
-              : {
-                  createdAt: 'desc'
-              }
+    include: {
+      academicSemester: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
   });
   const total = await prisma.semesterRegistration.count({
-      where: whereConditions
+    where: whereConditions,
   });
 
   return {
-      meta: {
-          total,
-          page,
-          limit
-      },
-      data: result
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
   };
 };
 
-const getByIdFromDB = async (id: string): Promise<SemesterRegistration | null> => {
+const getByIdFromDB = async (
+  id: string
+): Promise<SemesterRegistration | null> => {
   const result = await prisma.semesterRegistration.findUnique({
-      where: {
-          id
-      },
-      include: {
-          academicSemester: true
-      }
+    where: {
+      id,
+    },
+    include: {
+      academicSemester: true,
+    },
   });
   return result;
 };
 
+const updateOneToDB = async (
+  id: string,
+  payload: Partial<SemesterRegistration>
+): Promise<SemesterRegistration> => {
+  const isExist = await prisma.semesterRegistration.findUnique({
+    where: {
+      id,
+    },
+  });
 
-
-
-const updateOneToDB = async(id:string,payload:Partial<SemesterRegistration>):Promise<SemesterRegistration>=>{
-
-
-  const isExist =await prisma.semesterRegistration.findUnique({
-    where:{
-      id
-    }
-  })
-
-  if(!isExist){
-    throw new ApiError(httpStatus.BAD_REQUEST,"Data not found")
+  if (!isExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Data not found');
   }
 
-  if(payload?.status && isExist?.status === SemesterRegistrationStatus.UPCOMING && payload.status !== SemesterRegistrationStatus.ONGOING){
-    throw new ApiError(httpStatus.BAD_REQUEST,"Can only move from UPCOMING to ONGOING")
+  if (
+    payload?.status &&
+    isExist?.status === SemesterRegistrationStatus.UPCOMING &&
+    payload.status !== SemesterRegistrationStatus.ONGOING
+  ) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Can only move from UPCOMING to ONGOING'
+    );
   }
 
-  if(payload?.status && isExist?.status === SemesterRegistrationStatus.ONGOING && payload.status !== SemesterRegistrationStatus.ENDED){
-    throw new ApiError(httpStatus.BAD_REQUEST,"Can only move from ONGOING to ENDED")
+  if (
+    payload?.status &&
+    isExist?.status === SemesterRegistrationStatus.ONGOING &&
+    payload.status !== SemesterRegistrationStatus.ENDED
+  ) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Can only move from ONGOING to ENDED'
+    );
   }
- 
 
   const result = await prisma.semesterRegistration.update({
     where: {
-        id
+      id,
     },
     data: payload,
     include: {
-        academicSemester: true
-    }
-})
-  
-return result;
-}
+      academicSemester: true,
+    },
+  });
 
-
-
+  return result;
+};
 
 // start Regestration >>>
 
-const startMyRegistration  = async(authUserId:string)=>{
-
-// console.log(authUserId);
-const studentInfo = await prisma.student.findFirst({
-  where:{
-    studentID:authUserId
-  }
-})
-if(!studentInfo){
-  throw new ApiError(httpStatus.BAD_REQUEST,"Not Found Student")
-}
-const semesterRegistrationInfo = await prisma.semesterRegistration.findFirst({
-  where:{
-    status:{
-      in:[SemesterRegistrationStatus.ONGOING,SemesterRegistrationStatus.UPCOMING]
-    }
-  }
-})
-
-if(semesterRegistrationInfo?.status === SemesterRegistrationStatus.UPCOMING){
-  throw new ApiError(httpStatus.BAD_REQUEST,"Registration is not started yet")
-}
-
-const studentRegistration = await prisma.studentSemesterRegistration.create({
-  data:{
-    student:{
-      connect:{
-        id:studentInfo?.id
-      }
+const startMyRegistration = async (authUserId: string) => {
+  // console.log(authUserId);
+  const studentInfo = await prisma.student.findFirst({
+    where: {
+      studentID: authUserId,
     },
-    semesterRegistration:{
-      connect:{
-        id:semesterRegistrationInfo?.id
-      }
-    }
+  });
+  if (!studentInfo) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Not Found Student');
   }
-})
+  const semesterRegistrationInfo = await prisma.semesterRegistration.findFirst({
+    where: {
+      status: {
+        in: [
+          SemesterRegistrationStatus.ONGOING,
+          SemesterRegistrationStatus.UPCOMING,
+        ],
+      },
+    },
+  });
 
-return studentRegistration
-console.log(semesterRegistrationInfo);
-// console.log(studentInfo,"info");
+  if (
+    semesterRegistrationInfo?.status === SemesterRegistrationStatus.UPCOMING
+  ) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Registration is not started yet'
+    );
+  }
 
-}
+  let studentRegistration = await prisma.studentSemesterRegistration.findFirst({
+    where: {
+      student: {
+        id: studentInfo?.id,
+      },
+      semesterRegistration: {
+        id: semesterRegistrationInfo?.id,
+      },
+    },
+  });
 
+  if (!startMyRegistration) {
+    studentRegistration = await prisma.studentSemesterRegistration.create({
+      data: {
+        student: {
+          connect: {
+            id: studentInfo?.id,
+          },
+        },
+        semesterRegistration: {
+          connect: {
+            id: semesterRegistrationInfo?.id,
+          },
+        },
+      },
+    });
+  }
 
-export const SemesterRegistrationService = { insertDB,getAllFromDB,getByIdFromDB,updateOneToDB,startMyRegistration };
+  return studentRegistration;
+  // console.log(semesterRegistrationInfo);
+  // console.log(studentInfo,"info");
+};
+
+export const SemesterRegistrationService = {
+  insertDB,
+  getAllFromDB,
+  getByIdFromDB,
+  updateOneToDB,
+  startMyRegistration,
+};
