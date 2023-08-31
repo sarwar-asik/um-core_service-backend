02c40b018513108648d3baaa6918c14666a5f115
sarwar-asik <sarwarasik@gmail.com>
2023-08-31 14:34:15 +0600

    hasTimeConflict in utils

--- src/app/modules/courses/utils.ts
+++ src/app/modules/courses/utils.ts
@@ -1,11 +1,38 @@
 
 
-
 export const asyncForEach = async (array: any[], callback: any) => {
-    if(!Array.isArray(array)){
-    throw new Error("Expect an array")
-    }
+  if (!Array.isArray(array)) {
+    throw new Error('Expect an array');
+  }
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
-}
+};
+
+export const hasTimeConflict = (
+  existingSlots:{
+    startTime:string,
+    endTime:string,
+    dayOfWeak:string
+  }[],
+
+  newSlot:{
+    startTime:string,
+    endTime:string,
+    dayOfWeak:string
+  }
+) => {
+  for (const slot of existingSlots) {
+    const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`);
+    const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`);
+    const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
+    const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`);
+
+    if (newStartTime <= existingStartTime && newEndTime >= existingEndTime) {
+      // throw new ApiError(httpStatus.CONFLICT, 'The Room already booked  ');
+      // console.log('the room booked');
+      return true
+    }
+    return false
+  }
+};
--- src/app/modules/offeredCourseClassSchedules/offeredCourseClassSchedules.service.ts
+++ src/app/modules/offeredCourseClassSchedules/offeredCourseClassSchedules.service.ts
@@ -1,10 +1,64 @@
 import { OfferedCourseClassSchedule} from '@prisma/client';
 
 import prisma from '../../../shared/prisma';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
+import { hasTimeConflict } from '../courses/utils';
 
 const insertDB = async (
   data: OfferedCourseClassSchedule
 ): Promise<OfferedCourseClassSchedule> => {
+
+const alreadyBookedRoomOnDay = await prisma.offeredCourseClassSchedule.findMany({
+  where:{
+    dayOfWeek:data?.dayOfWeek,
+    room:{
+      id:data?.roomId
+    }
+  }
+})
+
+console.log(alreadyBookedRoomOnDay,'aaaaaaaaaa');
+
+const existingSlots = alreadyBookedRoomOnDay.map((schedules)=>({
+  startTime:schedules.startTime,
+  endTime:schedules.endTime,
+  dayOfWeak:schedules.dayOfWeek
+
+}))
+
+console.log(existingSlots,"exiiiiiiiii");
+const newSlot ={
+  startTime :data?.startTime,
+  endTime:data?.endTime,
+  dayOfWeak:data?.dayOfWeek
+
+}
+console.log(newSlot,"newwwwwwwwwwww");
+
+
+
+// for ( const  slot of existingSlots){
+//   const existingStartTime = new Date(`1970-01-01T${slot.startTime}:00`)
+//   const existingEndTime = new Date(`1970-01-01T${slot.endTime}:00`)
+//   const newStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`)
+//   const newEndTime = new Date(`1970-01-01T${newSlot.endTime}:00`)
+
+//   if(newStartTime  <= existingStartTime && newEndTime >= existingEndTime){
+//     throw new ApiError(httpStatus.CONFLICT,"The Room already booked  ")
+//     // console.log('the room booked');
+//   }
+
+
+// }
+
+
+if(hasTimeConflict(existingSlots,newSlot)){
+  throw new ApiError(httpStatus.CONFLICT,"The Room already booked  ")
+}
+
+
+
   const result = await prisma.offeredCourseClassSchedule.create({
     data,
     include:{
