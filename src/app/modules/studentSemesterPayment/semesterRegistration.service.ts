415ae532e6d372a0bfb9b982b90a78ac2724fa20
sarwar-asik <sarwarasik@gmail.com>
2023-09-09 12:31:32 +0600

    studentsemesterPyment.service.ts payment done

--- src/app/modules/studentSemesterPayment/semesterRegistration.service.ts
+++ src/app/modules/studentSemesterPayment/semesterRegistration.service.ts
@@ -3,7 +3,7 @@ import { DefaultArgs, PrismaClientOptions } from "@prisma/client/runtime/library
 
 const createSemesterPayment = async(
     //! Transaction prismaClient type declare !important
-    
+
     prismaClient:Omit<PrismaClient<PrismaClientOptions, never, DefaultArgs>, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends">,
     payload:{
     studentId:string,
@@ -12,6 +12,19 @@ const createSemesterPayment = async(
 })=>{
     // console.log("Semester payment",data1,data2);
     console.log(prismaClient,payload);
+    const dataToInsert = {
+        studentId:payload.studentId,
+        academicSemesterId:payload?.academicSemesterId,
+        fullPaymentAmount:payload?.totalPaymentAmount,
+        partialPaymentAmount:payload.totalPaymentAmount *0.5,
+        totalDueAmount:payload?.totalPaymentAmount,
+        totalPaidAmount:0
+
+    }
+    await prismaClient.studentSemesterPayment.create({
+        data:dataToInsert
+    })
+
 }
 
 export const StudentSemesterPaymentService = {createSemesterPayment}
