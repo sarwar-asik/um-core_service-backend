9f4416034df5761519d13b05d1a19bc54ef18f4d
sarwar-asik <sarwarasik@gmail.com>
2023-09-20 01:10:10 +0600

    added redis in redis.ts

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,13 @@ import { Server } from 'http';
 import app from './app';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
+import { RedisClient } from './shared/redis';
 
 async function mainFunc() {
+  // ! for redis ///
+  
+  await RedisClient.connect()
+
   const server: Server = app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`.green.underline.bold);
   });
